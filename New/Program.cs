6051f48661bc7fbb2ee52d7409fbc3a9748d06f8
Egor <sychov1991@mail.ru>
2024-09-10 23:40:04 +0500

    Add project files.

---
+++ New/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace New
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var someClass = new SomeClass();
+
+            var task = someClass.SomeAsync();
+            var result = await task;
+
+            Console.ReadLine();
+        }
+    }
+}
